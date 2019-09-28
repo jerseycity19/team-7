12163957a96d0bce53254e385c26548de1057038
Anthony Bravo <abravo3641@gmail.com>
2019-09-28 08:59:45 -0400

    added homepage

--- mainapp/src/Components/Dashboard/Dashboard.js
+++ mainapp/src/Components/Dashboard/Dashboard.js
@@ -50,8 +50,8 @@ function Testimonials() {
 			<section>
 				<h2>Testimonials</h2>
 				<p>"This is the best platform ever to talk to people around the world"</p>
-				<p>"This is the best platform ever to talk to people around the world"</p>
-				<p>"This is the best platform ever to talk to people around the world"</p>
+				<p>"I could not believe the people I have been able to meet because of this platform"</p>
+				<p>"I really see my self using this in the future"</p>
 			</section>
 		</div>
 	)
@@ -61,7 +61,7 @@ function Contact(){
 	return(
 		<div class="Contact">
 			<section>
-				<h2>Conctact Us!</h2>
+				<h2>Contact Us!</h2>
 				<p> <span class="button"> Facebook </span> </p>
 				<p> <span class="button"> Twitter </span> </p>
 			</section>
