6f3de8dcf9f3223b632010bbd1f47269170b8008
Andrew Rodrigue <arodr95@lsu.edu>
2019-09-28 04:17:27 -0500

    updated occupancy counter

--- mainapp/src/Components/Chatroom/Chatroom.js
+++ mainapp/src/Components/Chatroom/Chatroom.js
@@ -3,21 +3,30 @@ import React, { useState } from 'react';
 import './Chatroom.css';
 import NavBar from '../NavBar'
 
+// function Counter(){
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <button onClick={() => setCount(count + 1)}>
+//       </button>
+//     </div>
+//   );
+// }
 
 function Chatroom() {
-  const [trendingTopics, setTrendingTopics] = useState(["News1", "News2", "News3", "News4", "News5", "News6", "News7"]);
-  console.log(trendingTopics.length);
+  const [trendingTopics, setTrendingTopics] = useState([{title: "News1",count:0} ,{title: "News2",count:0}, {title: "News3",count:0}, {title: "News4",count:0}, {title: "News5",count:0}, {title: "News6",count:0}, {title: "News7",count:0}]);
   return (
     <div className="Chatroom">
       <NavBar />
       <ul className="roomList">
           {trendingTopics.map(i => {
-            return <Room roomName={i}></Room>
+            return <Room roomName={i.title} roomCount={i.count}></Room>
             })
           }
       </ul>
       <button className="addRoom" onClick={() => {
-        trendingTopics.push("test"); 
+        trendingTopics.push({title:"test", count:0} ); 
         setTrendingTopics(() => {
           var temp = trendingTopics.slice();
           return temp;
@@ -28,20 +37,25 @@ function Chatroom() {
   );
 }
 
+
 class Room extends React.Component {
   constructor(props) {
     super(props);
     let name = props.roomName;
     this.state = { //React garantees this will live throughout our object. This object lives. 
-      roomName: props.roomName
+      roomName: props.roomName, 
+      roomCount: props.roomCount
     }
   }
 
   render() {
     return(
       <li>
-          <p className="room"><span>{this.state.roomName}</span><button type="button">Start</button></p>
-          <p>In Room: 0</p>
+          <p className="room"><span>{this.state.roomName}</span><button type="button" onClick= {() =>{ 
+              this.setState({roomCount: this.state.roomCount+1});
+              }
+            } >Join</button></p>          
+          <p>In room: {this.state.roomCount}</p>
           <p>Duration: 0:00</p>
       </li>
     );
