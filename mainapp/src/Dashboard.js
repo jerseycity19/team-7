d57bb65d2994f517b240b7ae7ce2feebb4028daa
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2019-09-27 22:00:52 -0400

    Navigation bar is set

---
+++ mainapp/src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav } from "tabler-react";
+
+const Dashboard = props => {
+    return (
+        <Nav>
+            <Nav.Item value="Dashboard" icon="globe" to="/">
+            </Nav.Item>
+        </Nav>
+    )
+};
+
+export default Dashboard;
