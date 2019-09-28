410f9305068f28619ca2f4fcba4f617e2cf63e5c
Morgan Brenner <mbrenner@stevens.edu>
2019-09-28 02:04:57 -0400

    Added trending news component

--- mainapp/src/App.js
+++ mainapp/src/App.js
@@ -4,6 +4,7 @@ import {Switch, Redirect, Route} from 'react-router-dom';
 
 import Dashboard from './Dashboard';
 import Chatroom from './Components/Chatroom/Chatroom';
+import Trending_News from './Components/Trending_News/Trending_News'
 
 import "tabler-react/dist/Tabler.css";
 import './App.css';
@@ -15,6 +16,7 @@ const App = props => {
     <div>
     <Switch>
       <Route path="/chatroom" component={Chatroom} />
+      <Route path="/trending" component={Trending_News} />
       <Route path="/" component={Dashboard} />
     </Switch>
 
