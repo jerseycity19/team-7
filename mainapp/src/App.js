a9320a0129321934709da95b71a0cec5da4e8fd7
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2019-09-27 23:59:54 -0400

    Connected to Nav Bar

--- mainapp/src/App.js
+++ mainapp/src/App.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Switch, Redirect, Route} from 'react-router-dom';
 
 import Dashboard from './Dashboard';
+import Chatroom from './Chatroom/Chatroom';
 
 import "tabler-react/dist/Tabler.css";
 import './App.css';
@@ -9,6 +10,7 @@ import './App.css';
 const App = props => {
   return (
     <Switch>
+      <Route path="/chatroom" component={Chatroom} />
       <Route path="/" component={Dashboard} />
     </Switch>
   );
