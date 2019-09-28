7a86636829917c0b339eabef0093bf33f1991b76
Morgan Brenner <mbrenner@stevens.edu>
2019-09-28 06:57:00 -0400

    Added hangouts link to start buttons

--- mainapp/src/Components/Chatroom/Chatroom.js
+++ mainapp/src/Components/Chatroom/Chatroom.js
@@ -3,30 +3,21 @@ import React, { useState } from 'react';
 import './Chatroom.css';
 import NavBar from '../NavBar'
 
-// function Counter(){
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <button onClick={() => setCount(count + 1)}>
-//       </button>
-//     </div>
-//   );
-// }
-
 function Chatroom() {
-  const [trendingTopics, setTrendingTopics] = useState([{title: "News1",count:0} ,{title: "News2",count:0}, {title: "News3",count:0}, {title: "News4",count:0}, {title: "News5",count:0}, {title: "News6",count:0}, {title: "News7",count:0}]);
+  const hangoutLink = 'http://hangouts.google.com/start';
+  const [trendingTopics, setTrendingTopics] = useState([{title: "News1", link: hangoutLink, count:0} ,{title: "News2", link: hangoutLink, count:0}, {title: "News3", link: hangoutLink, count:0}, {title: "News4", link: hangoutLink, count:0}, {title: "News5", link: hangoutLink, count:0}, {title: "News6", link: hangoutLink, count:0}, {title: "News7", link: hangoutLink, count:0}]);
   return (
     <div className="Chatroom">
       <NavBar />
+      <h1>Get the conversation started!</h1>
       <ul className="roomList">
           {trendingTopics.map(i => {
-            return <Room roomName={i.title} roomCount={i.count}></Room>
+            return <Room roomName={i.title} roomCount={i.count} link={i.link}></Room>
             })
           }
       </ul>
       <button className="addRoom" onClick={() => {
-        trendingTopics.push({title:"test", count:0} ); 
+        trendingTopics.push({title:"test", link: hangoutLink, count:0} ); 
         setTrendingTopics(() => {
           var temp = trendingTopics.slice();
           return temp;
@@ -44,17 +35,20 @@ class Room extends React.Component {
     let name = props.roomName;
     this.state = { //React garantees this will live throughout our object. This object lives. 
       roomName: props.roomName, 
-      roomCount: props.roomCount
+      roomCount: props.roomCount,
+      link: props.link
     }
   }
 
   render() {
     return(
       <li>
-          <p className="room"><span>{this.state.roomName}</span><button type="button" onClick= {() =>{ 
+          <p className="room"><span>{this.state.roomName}</span><button type="button" onClick= {() =>{
+              var win = window.open(this.state.link);
+              this.setState({link: win.location.href});
               this.setState({roomCount: this.state.roomCount+1});
               }
-            } >Join</button></p>          
+            } >Start</button></p>          
           <p>In room: {this.state.roomCount}</p>
           <p>Duration: 0:00</p>
       </li>
